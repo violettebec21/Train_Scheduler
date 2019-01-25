cb8c28d449264f92e951e089db6ed3d98db2e348
violettebec <bechoerner@gmail.com>
2019-01-25 00:18:15 -0600

    revising js file and HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,138 +1,94 @@
+//--------------------------------------------------------------------------------------------
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBCpHP0-JYza3kWogRG4LtbArYMKizqAbg",
+    authDomain: "mycoolerdemo.firebaseapp.com",
+    databaseURL: "https://mycoolerdemo.firebaseio.com",
+    projectId: "mycoolerdemo",
+    storageBucket: "mycoolerdemo.appspot.com",
+    messagingSenderId: "938477946827"
+  };
+  firebase.initializeApp(config);
 
-console.log("sanity");
+    // Create a variable to reference the database.
+    var database = firebase.database();
 
-$(document).ready(function () {
+    // Initial Values
+    var name = "";
+    var destination = "";
+    var firstTrain = 0;
+    var frequency = "";
 
-    //using from class activity "click json" and "button triggered ajax" for AJAX call, JSON, array of items, and dynamic button population
+    // Capture Button Click
+    $("#add-user").on("click", function (event) {
+      event.preventDefault();
 
-    // display initial array of tvShows
-    var tvShows = ["The Office", "Mad Men", "Friends", "Unbreakable Kimmy Schmidt", "30 Rock", "Ricky and Morty", "South Park", "Spongebob", "Schitt's Creek", "Silicon Valley", "The Walking Dead", "The Middle", "Black Mirror"];
+      // Grabbed values from text-boxes
+      name = $("#name-input").val().trim();
+      destination = $("#destination-input").val().trim();
+      firstTrain = $("#firstTrain-input").val().trim();
+      frequency = $("#frequency-input").val().trim();
 
-    // Function for dumping the JSON content for each button into the div
-    function displaytvShowInfo() {
+      // Code for "Setting values in the database"
+      database.ref().set({
+        name: name,
+        destination: destination,
+        firstTrain: firstTrain,
+        frequency: frequency
+      });
 
-        var tvShows = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + tvShows + "&api_key=8I87mPazwmPjBxwELpInl2BU9Wp4C1OB";
-
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            var results = response.data;
-
-            // Looping over every result item
-            for (var i = 0; i < results.length; i++) {
-
-                // Only taking action if the photo has an appropriate rating
-                if (results[i].rating !== "r") {
-
-                    // Creating a div for the gif
-                    //example of JQuery selector on HTML-- dynamically created HTML element
-                    var gifDiv = $("<div>");
-
-                    // Storing the result item's rating
-                    var rating = results[i].rating;
-
-                    // Creating a paragraph tag with the result item's rating
-                    var p = $("<p>").text("Rating: " + rating);
-
-                    // Creating an image tag
-                    var tvShowImage = $("<img>");
-
-                    // Giving the image tag an src attribute of a property pulled off the result item
-                    tvShowImage.attr("src", results[i].images.fixed_height.url);
-
-                    // Appending the paragraph and tvShowImage we created to the "gifDiv" div we created
-                    gifDiv.append(p);
-                    gifDiv.append(tvShowImage);
-
-                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                    $("#tvShows-view").prepend(gifDiv);
-
-                }
-            }
-
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
-
-    // Function for displaying tvShow data
-    function renderButtons() {
-
-        // Deleting the buttons prior to adding new tvShows
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-
-        // Looping through the array of tvShows
-        for (var i = 0; i < tvShows.length; i++) {
-
-            // Then dynamically generating buttons for each tvShow in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-            var a = $("<button>");
-            // Adding a class of tvShow to our button
-            a.addClass("tvShow");
-            // Adding a data-attribute
-            a.attr("data-name", tvShows[i]);
-            // Providing the initial button text
-            a.text(tvShows[i]);
-            // Adding the button to the buttons-view div
-            $("#buttons-view").append(a);
-        }
-    }
-    renderButtons();
-
-    // This function handles events where one button is clicked
-    $("#add-tvShow").on("click", function (event) {
-        event.preventDefault();
-
-        // This line grabs the input from the textbox
-        var tvShow = $("#tvShow-input").val().trim();
-
-        // Adding the tvShow from the textbox to our array
-        tvShows.push(tvShow);
-        console.log(tvShows);
-
-        // Calling renderButtons which handles the processing of our tvShow array
-        renderButtons();
     });
 
-    // Function for displaying the tvShow info
-    // Using $(document).on instead of $(".tvShow").on to add event listeners to dynamically generated elements
-    $(document).on("click", ".tvShow", displaytvShowInfo);
+    // Firebase watcher + initial loader HINT: .on("value")
+    database.ref().on("value", function (snapshot) {
+
+      // Log everything that's coming out of snapshot
+      console.log(snapshot.val());
+      console.log(snapshot.val().name);
+      console.log(snapshot.val().destination);
+      console.log(snapshot.val().firstTrain);
+      console.log(snapshot.val().frequency);
+
+      // Change the HTML to reflect
+      $("#name-display").text(snapshot.val().name);
+      $("#destination-display").text(snapshot.val().destination);
+      $("#firstTrain-display").text(snapshot.val().firstTrain);
+      $("#frequency-display").text(snapshot.val().frequency);
+
+      // Handle the errors
+    }, function (errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
 
 
-    // Calling the renderButtons function to display the initial buttons
-    renderButtons();
+    //MOMENT.JS code----------------------------------------------------
 
-    //placing document.onclick with function AFTER my calling my renderButtons(); so this action happens AFTER my array and everything is processed
-    $(document).on("click", ".gif", playPauseGif);
+    var randomDate = "02/23/1999";
+    var randomFormat = "MM/DD/YYYY";
+    var convertedDate = moment(randomDate, randomFormat);
 
-    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
-    var state = $(this).attr("data-state");
-    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-    // Then, set the image's data-state to animate
-    // Else set src to the data-still value
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-    } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-    }
-});
+    // Using scripts from moment.js write code below to complete each of the following.
+    // Console.log to confirm the code changes you made worked.
 
+    // 1 ...to convert the randomDate into three other date formats
+    console.log(convertedDate.format("MM/DD/YY"));
+    console.log(convertedDate.format("MMM Do, YYYY hh:mm:ss"));
+    console.log(convertedDate.format("X"));
 
-//notes from tutor session----------------
-//below is how we would use .catch to log the specific error message
+    // 2 ...to determine the time in years, months, days between today and the randomDate
+    console.log(convertedDate.toNow());
+    console.log(convertedDate.diff(moment(), "years"));
+    console.log(convertedDate.diff(moment(), "months"));
+    console.log(convertedDate.diff(moment(), "days"));
 
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//     $("#tvShows-view").text(JSON.stringify(response));
-//   }).catch(function(err){
-//       console.log(err);
-//   });
-// }
+    // 3 ...to determine the number of days between the randomDate and 02/14/2001
+    var newDate = moment("02/14/2001", randomFormat);
+    console.log(convertedDate.diff(newDate, "days"));
+
+    // 4 ...to convert the randomDate to unix time (be sure to look up what unix time even is!!!)
+    console.log(convertedDate.format("X"));
+
+    // 5 ...to determine what day of the week and what week of the year this randomDate falls on.
+    console.log(convertedDate.format("DDD"));
+    console.log(convertedDate.format("dddd"));
+
