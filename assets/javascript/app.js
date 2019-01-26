adbddcaf3164039a5c68006066be143f1e8b7a4d
violettebec <bechoerner@gmail.com>
2019-01-25 23:59:07 -0600

    adding updated js and html files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,94 +1,99 @@
 //--------------------------------------------------------------------------------------------
-  // Initialize Firebase
-  var config = {
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyBCpHP0-JYza3kWogRG4LtbArYMKizqAbg",
     authDomain: "mycoolerdemo.firebaseapp.com",
     databaseURL: "https://mycoolerdemo.firebaseio.com",
     projectId: "mycoolerdemo",
     storageBucket: "mycoolerdemo.appspot.com",
     messagingSenderId: "938477946827"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-    // Create a variable to reference the database.
-    var database = firebase.database();
+// Create a variable to reference the database.
+var dataRef = firebase.database();
 
-    // Initial Values
-    var name = "";
-    var destination = "";
-    var firstTrain = 0;
-    var frequency = "";
+// Initial Values
+var trainName = "";
+var destination = "";
+var firstTrain = 0;
+var frequency = "";
 
-    // Capture Button Click
-    $("#add-user").on("click", function (event) {
-      event.preventDefault();
+// Capture Button Click
+$("#add-user").on("click", function (event) {
+    event.preventDefault();
 
-      // Grabbed values from text-boxes
-      name = $("#name-input").val().trim();
-      destination = $("#destination-input").val().trim();
-      firstTrain = $("#firstTrain-input").val().trim();
-      frequency = $("#frequency-input").val().trim();
+    // Grabs values from text-boxes
+    trainName = $("#trainName-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrain = moment($('#firstTrain-input').val().trim(), "HH:mm").format("");
+    frequency = $("#frequency-input").val().trim();
 
-      // Code for "Setting values in the database"
-      database.ref().set({
-        name: name,
-        destination: destination,
-        firstTrain: firstTrain,
-        frequency: frequency
-      });
+    // Code for the push TO firebase; object holding key value pairs to push to firebase
+    dataRef.ref().push({
+
+        name: trainName,
+        tdestination: destination,
+        tFirst: firstTrain,
+        tfreq: frequency,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
 
     });
 
-    // Firebase watcher + initial loader HINT: .on("value")
-    database.ref().on("value", function (snapshot) {
+});
 
-      // Log everything that's coming out of snapshot
-      console.log(snapshot.val());
-      console.log(snapshot.val().name);
-      console.log(snapshot.val().destination);
-      console.log(snapshot.val().firstTrain);
-      console.log(snapshot.val().frequency);
+// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+dataRef.ref().on("child_added", function (childSnapshot) {
 
-      // Change the HTML to reflect
-      $("#name-display").text(snapshot.val().name);
-      $("#destination-display").text(snapshot.val().destination);
-      $("#firstTrain-display").text(snapshot.val().firstTrain);
-      $("#frequency-display").text(snapshot.val().frequency);
+    var trainName = childSnapshot.val().name;
+    var destination = childSnapshot.val().tdestination;
+    var firstTrain = childSnapshot.val().tFirst;
+    var frequency = childSnapshot.val().tfreq;
 
-      // Handle the errors
-    }, function (errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
+    // Log everything that's coming out of snapshot
+    console.log("///////////////////////////////");
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().frequency);
 
 
-    //MOMENT.JS code----------------------------------------------------
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
-    var randomDate = "02/23/1999";
-    var randomFormat = "MM/DD/YYYY";
-    var convertedDate = moment(randomDate, randomFormat);
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-    // Using scripts from moment.js write code below to complete each of the following.
-    // Console.log to confirm the code changes you made worked.
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-    // 1 ...to convert the randomDate into three other date formats
-    console.log(convertedDate.format("MM/DD/YY"));
-    console.log(convertedDate.format("MMM Do, YYYY hh:mm:ss"));
-    console.log(convertedDate.format("X"));
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
 
-    // 2 ...to determine the time in years, months, days between today and the randomDate
-    console.log(convertedDate.toNow());
-    console.log(convertedDate.diff(moment(), "years"));
-    console.log(convertedDate.diff(moment(), "months"));
-    console.log(convertedDate.diff(moment(), "days"));
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-    // 3 ...to determine the number of days between the randomDate and 02/14/2001
-    var newDate = moment("02/14/2001", randomFormat);
-    console.log(convertedDate.diff(newDate, "days"));
+    //next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainConverted = moment(nextTrain).format("hh:mm a");
+    // console.log("ARRIVAL TIME: " + moment(nextTrain).format("HH:mm"));
 
-    // 4 ...to convert the randomDate to unix time (be sure to look up what unix time even is!!!)
-    console.log(convertedDate.format("X"));
+    // full list of items to the well
+    $("#current-trains").append("<div class='well'><span class='trainName'> " + childSnapshot.val().trainName +
+        " </span><span class='destination'> " + childSnapshot.val().destination +
+        " </span><span class='firstTrain'> " + childSnapshot.val().firstTrain +
+        " </span><span class='frequency'> " + childSnapshot.val().frequency +
+        " </span></div>");
 
-    // 5 ...to determine what day of the week and what week of the year this randomDate falls on.
-    console.log(convertedDate.format("DDD"));
-    console.log(convertedDate.format("dddd"));
+    $("#trainTable > tbody").append("<tr><td>" + trainName + "</td><td>" + destination + "</td><td>" + "Every " + frequency + " minutes" + "</td><td>" + nextTrainConverted + "</td><td>" + tMinutesTillTrain + "</td></tr>");
+
+
+    // Handle the errors
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
 
